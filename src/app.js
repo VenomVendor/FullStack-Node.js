6ca0e4c3e8090d98737eb0ccb4d0cc7e2168a070
VenomVendor <info@VenomVendor.com>
2016-02-28 19:13:30 +0530

    Update main node js from ES5 to ES6 Fix eslint issues.

--- src/app.js
+++ src/app.js
@@ -1,73 +1,55 @@
-'use strict';
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import cons from 'consolidate';
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import favicon from 'serve-favicon';
+import fs from 'fs';
+import http from 'http';
+import logger from 'morgan';
+import path from 'path';
+import responseTime from 'response-time';
+import index from './routes/index';
+import demo from './routes/demo';
+import get from './routes/read';
+import constants from './utils/constants';
 
-var constants = require('./utils/constants');
-var _version = '/' + constants.API._VERSION;
-//var async = require('async');
-var bodyParser = require('body-parser');
-var compression = require('compression');
-var cons = require('consolidate');
-var cookieParser = require('cookie-parser');
-var express = require('express');
-var favicon = require('serve-favicon');
-var fs = require('fs');
-var http = require('http');
-var logger = require('morgan');
-var multer = require('multer');
-var path = require('path');
-var responseTime = require('response-time');
+const logDir = `${__dirname}/../logs/`;
+const accessLog = 'access.log';
+const loggerFormat = '[:date] [:method :status HTTP/:http-version] - :url';
 
-var logDir = __dirname + '/../logs/';
-var accessLog = 'access.log';
-var loggerFormat = '[:date] [:method :status HTTP/:http-version] - :url';
-var _dirname = path.join(__dirname, '../build');
-var PORT = 3030;
+const _version = `/${constants.API._VERSION}`;
+const _dirname = path.join(__dirname, '../build');
+const PORT = 3030;
 
 /* current date */
-logger.date = function() {
-    return (new Date()).getTime();
-};
+logger.date = () => new Date().getTime();
 
+/* eslint no-extend-native: 0 */
 Object.defineProperty(String.prototype, 'isEmpty', {
-    value: function() {
+    value() {
         return this.length === 0;
     }
 });
 
 Object.defineProperty(String.prototype, 'isTooEmpty', {
-    value: function() {
+    value() {
         return this.isEmpty() || this.trim().length === 0;
     }
 });
 
-/*##################################################
- ############### CONFIGURE API {START} ##############
- ###################################################*/
-
-var index = require('./routes/index');
-var get = require('./routes/read');
-var demo = require('./routes/demo');
-
-/*##################################################
- ############### CONFIGURE API {END} ################
- ###################################################*/
-
-var app = express();
-
-app.set('port', PORT);
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(_dirname, 'views')); // view engine setup
-app.engine('html', cons.swig);
-
-var start = process.hrtime();
-
-var sendError = function(req, res, status) {
-    var callback = req.query.callback || null;
-    var data = {
+const app = express();
+const start = process.hrtime();
+const accessLogStream = fs.createWriteStream(logDir + accessLog, {
+    flags: 'a'
+});
+const sendError = (req, res, status) => {
+    const callback = req.query.callback || null;
+    const data = {
         status: constants.ERROR,
         message: http.STATUS_CODES[status] || constants.UNKNOWN_ERROR
     };
-    if(callback) {
+    if (callback) {
         data.callback = callback;
     }
     res.set('content-type');
@@ -76,90 +58,89 @@ var sendError = function(req, res, status) {
     res.end();
 };
 
-var accessLogStream = fs.createWriteStream(logDir + accessLog, {
-    flags: 'a'
-});
+/* *********** Express settings START ************** */
 
-app.use(logger(loggerFormat, {
-    stream: accessLogStream
-}));
+app.set('port', PORT);
+// view engine setup
+app.set('view engine', 'ejs');
+app.set('views', path.join(_dirname, 'views'));
+app.engine('html', cons.swig);
 
-app.use(logger('[:date] [:method :status HTTP/:http-version] - :url '));
 app.use(express.static(path.join(_dirname, 'public')));
-app.use(favicon(_dirname + '/public/images/favicon-mongo.ico'));
+app.use(favicon(`${_dirname}/public/images/favicon-mongo.ico`));
+app.use(logger(loggerFormat, { stream: accessLogStream }));
 
-app.use(bodyParser.raw({ // parse some custom thing into a Buffer
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({
     type: 'application/vnd.custom-type'
 }));
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.text({ // parse an HTML body into a string
+// for parsing application/json
+app.use(bodyParser.json());
+
+// parse an HTML body into a string
+app.use(bodyParser.text({
     type: 'text/html'
 }));
 
-app.use(bodyParser.urlencoded({ // for parsing application/x-www-form-urlencoded
+// for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
     extended: true
 }));
 
 app.use(cookieParser());
-//app.use(multer()); // for parsing multipart/form-data
-app.use(responseTime()); // detect Response time.
+// app.use(multer()); // for parsing multipart/form-data
+
+// detect Response time.
+app.use(responseTime());
+
+// compress responses.
 app.use(compression({
     level: 9
-})); // compress responses.
+}));
+
+/* *********** Express settings END ************** */
 
 console.log(process.hrtime(start));
 
-/***************************************************
- ***************** SET API START ********************
- ****************************************************/
-// insert this before your routes
-app.use(function(req, res, next) {
-    for (var key in req.query) {
-        if(req.query.hasOwnProperty(key)) {
-            req.query[key.toLowerCase()] = req.query[key];
-        }
-    }
-    next();
-});
+/* ************ SERVE STATIC START **************** */
 
 app.use('/js', express.static(path.join(_dirname, 'public', 'javascripts')));
 app.use('/css', express.static(path.join(_dirname, 'public', 'stylesheets')));
 app.use('/img', express.static(path.join(_dirname, 'public', 'images')));
 app.use('', express.static(path.join(_dirname, 'public', 'htmls')));
 
-app.use(_version + '/', index);
-app.use(_version + '/get/', get);
-app.use(_version + '/demo/', demo);
+app.use(`${_version}/`, index);
+app.use(`${_version}/get/`, get);
+app.use(`${_version}/demo/`, demo);
 
-/***************************************************
- ***************** SET API END **********************
- ****************************************************/
+/* ************* SERVE STATIC END ***************** */
 
-/*############### ERROR HANDLERS ###############*/
-app.all('/:filename/', function(req, res) {
-    res.redirect(301, _version + '/' + req.params.filename);
+/* **************** ROUTING STARTS ***************** */
+
+app.all('/:filename/', (req, res) => {
+    res.redirect(301, `${_version}/${req.params.filename}`);
 });
 
-app.get(_version + '/ejs/*/*', function(req, res) {
-    var path = req.url;
+app.get(`${_version}/ejs/*/*`, (req, res) => {
+    const url = req.url;
     res.render('error', {
-        pageName: path
+        pageName: url
     });
 });
 
-app.get(_version + '/ejs/*', function(req, res) {
-    var path = req.url;
+app.get(`${_version}/ejs/*`, (req, res) => {
+    const url = req.url;
     res.render('index', {
-        pageName: path
+        pageName: url
     });
 });
 
-app.get(_version + '/:filename', function(req, res) {
-    var fileName = req.params.filename;
-    if(fileName.endsWith('.html')) {
-        fs.stat(path.join(_dirname, 'public', 'htmls', fileName), function(err) {
-            if(err === null) {
+app.get(`${_version}/:filename`, (req, res) => {
+    const fileName = req.params.filename;
+    if (fileName.endsWith('.html')) {
+        fs.stat(path.join(_dirname, 'public', 'htmls', fileName), err => {
+            if (err === null) {
                 res.sendFile(path.join(_dirname, 'public', 'htmls', fileName));
             } else {
                 res.sendFile(path.join(_dirname, 'public', 'htmls', 'non-index.html'));
@@ -169,46 +150,44 @@ app.get(_version + '/:filename', function(req, res) {
         sendError(req, res, 404);
     }
 });
+/* **************** ROUTING ENDS ***************** */
 
+/* ############### ERROR HANDLERS STARTS ############### */
 // Handler for internal server errors
-/*jshint unused: false, node: true */
-app.use(function errorHandler(err, req, res, next) {
-    if(app.get('env') === 'development') {
+const errorHandler = (err, req, res, next) => {
+    if (app.get('env') === 'development') {
         console.error(err.message);
         console.error(err.stack);
     }
     sendError(req, res, 500);
-});
+    next();
+};
 
 // catch 404 and forward to error handler.
-// Should be added last.
-app.use(function notFound(req, res) {
+const notFound = (req, res) => {
     sendError(req, res, 404);
-});
+};
 
-// Same as 404
-// app.all('*', function (req, res) {
-//     res.set('content-type');
-//     var data = {
-//         status: 'error',
-//         message: 'Page Not Found.'
-//     };
-//     res.status(404);
-//     res.json(data);
-// });
+app.use(errorHandler);
 
-/*############### ERROR HANDLERS ###############*/
+// Should be added last.
+app.use(notFound);
+/* ############### ERROR HANDLERS END ############### */
 
-module.exports = app;
+export default app;
 
-var server = http.createServer(app);
+/* ###############SERVER############### */
+const server = http.createServer(app);
 
-server.listen(app.get('port'), function(err) {
-    if(err) {
+server.listen(app.get('port'), err => {
+    if (err) {
         throw err;
     }
-    //var os = require('os');
-    //var networkInterfaces = os.networkInterfaces();
-    var ipAddress = 'localhost';
-    console.log('\x1b[31m', '\n Server running at\thttp://' + ipAddress + ':' + app.get('port') + '\n');
+    // var os = require('os');
+    // var networkInterfaces = os.networkInterfaces();
+    const ipAddress = 'localhost';
+    console.log(
+        '\x1b[31m',
+        `\n Server running at\thttp://${ipAddress}:${app.get('port')}\n`
+    );
 });
